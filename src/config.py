fca7c99bac4338205f227be1277c89cf97867123
KindaRaf <nalupta07@yahoo.com>
2024-05-23 14:16:06 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # Refer to the instructions in the README
 
 # Required
-token = ""        # Telegram bot token
+token = "6599518132:AAHLft16_B3YnDalMUMBl5OrfCSgBPy6uig"        # Telegram bot token
 credentials = ""  # Path to credentials JSON file
 spreadsheet = ""  # Name of Google Sheet
 channel = ""      # Unique identifier of your channel/chat
