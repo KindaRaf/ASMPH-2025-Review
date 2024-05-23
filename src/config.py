0f25fbb62c10de657ce0f01d564522e76779657b
KindaRaf <nalupta07@yahoo.com>
2024-05-23 14:49:39 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@
 # Required
 token = "6599518132:AAHLft16_B3YnDalMUMBl5OrfCSgBPy6uig"        # Telegram bot token
 credentials = "src/key"  # Path to credentials JSON file
-spreadsheet = ""  # Name of Google Sheet
-channel = ""      # Unique identifier of your channel/chat
+spreadsheet = "QUIZ Source"  # Name of Google Sheet
+channel = "+Cxb0X9vU5BE1ZTA1"      # Unique identifier of your channel/chat
 
 # Optional
 anonymous = True  # Whether to keep  responses anonymous
